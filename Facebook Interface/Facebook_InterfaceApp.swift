926e24f9e134da5aadcf0b84cc80511a8ebee63a
icaroliveira <icarogin2@gmail.com>
2023-04-25 15:36:08 -0400

    Initial Commit

---
+++ Facebook Interface/Facebook_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facebook_InterfaceApp.swift
+//  Facebook Interface
+//
+//  Created by icaro on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Facebook_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
