0abce220e2fabd09dd922e367bb2f7293b8b9b03
icaroliveira <icarogin2@gmail.com>
2023-04-25 15:58:15 -0400

    Interface do Facebook tanto para celulares quanto para tablets também

--- Facebook Interface/ContentView.swift
+++ Facebook Interface/ContentView.swift
@@ -8,15 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.horizontalSizeClass) var HSC
+    
+    @State var Email: String = ""
+    @State var Passowrd: String = ""
+    
     var body: some View {
+        ZStack {
+            
+            Color("BackgroundColor").ignoresSafeArea()
+            
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: self.HSC == .compact ? 300 : 550)
+            
+            Spacer().frame(height: self.HSC == .compact ? 64 : 120)
+            
+        VStack {
+                TextField("Email", text: $Email)
+                    .padding()
+                    .frame(width: self.HSC == .compact ? 370 : 650, height: self.HSC == .compact ? 65 : 90)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .padding(.top, 24)
+                    .padding(.bottom, 12)
+                    .keyboardType(.emailAddress)
+                    
+                SecureField("Passowrd", text: $Passowrd)
+                    .padding()
+                    .frame(width: self.HSC == .compact ? 370 : 650, height: self.HSC == .compact ? 65 : 90)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .padding(.top, 24)
+                    .padding(.bottom, 12)
+                
+            Spacer().frame(height: 40)
+            
+            Button(action:{}) {
+                Text("Login")
+                    .font(.title3)
+                    .foregroundColor(Color.white)
+                    .bold()
+                    .padding(20)
+                    .frame(width: self.HSC == .compact ? 370 : 650, height: self.HSC == .compact ? 65 : 90)
+                    .background(Color("ButtonColor"))
+                    .cornerRadius(20) }
+            
+            }
+            .padding(.horizontal)
+            
+            Spacer().frame(height: 64)
+            
+        HStack{
+                Text("Don't have an account?")
+                    .foregroundColor(.white)
+                    .font(.custom("Avenir", size: self.HSC == .compact ? 19 : 24))
+                Button("Sign up") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                    .foregroundColor(Color("TextColor"))
+                    .font(.custom("Avenir", size: self.HSC == .compact ? 19 : 24))
+            }
+            Button("Forgot your password") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                .foregroundColor(Color("TextColor"))
+                .font(.custom("Avenir", size: self.HSC == .compact ? 19 : 24))
+ 
         }
-        .padding()
+        .padding(.horizontal)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
